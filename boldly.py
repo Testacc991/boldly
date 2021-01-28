8ee05d671502b6a43feb326d13b9077bddb14f2f
Testacc991 <70212365+Testacc991@users.noreply.github.com>
2021-01-28 17:18:10 +0200

    add comments

--- boldly.py
+++ boldly.py
@@ -6,65 +6,65 @@ import random
 
 import click
 import flickrapi
-import halftone
+import halftone 
 from mastodon import Mastodon
-from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from PIL import Image, ImageDraw, ImageFont, ImageFilter             # Модуль для маніпулювання зображеннями
 import requests
-import tweepy
+import tweepy # API твіттера
 
-with open('config.json') as f:
+with open('config.json') as f:                                       # Завантажуємо конфіг файл
     config = json.load(f)
-apikey = config['flickrkey']
+apikey = config['flickrkey']                                         # Отримую доступ до акаунту flickr
 apisecret = config['flickrsecret']
 flickr = flickrapi.FlickrAPI(apikey, apisecret, format='parsed-json')
 
-mastodon = Mastodon(
+mastodon = Mastodon(                                                 # Отримую доступ до акаунту mastodon
     client_id=config['mast_client'],
     client_secret=config['mast_secret'],
     access_token=config['mast_key'],
     api_base_url=config['mast_base_url']
 )
 
-twconf = config['twitter']
+twconf = config['twitter']                                          # Отримую доступ до аккаунту twitter
 
 twauth = tweepy.OAuthHandler(twconf['consumer_key'], twconf['consumer_secret'])
 twauth.set_access_token(twconf['access_token'], twconf['access_secret'])
 
 twapi = tweepy.API(twauth)
 
-def get_image(width, height, word=''):
+def get_image(width, height, word=''):                             # Функція щоб добути фото              
     if not word:
-        with open('words.txt') as f:
+        with open('words.txt') as f:                               # Обираю випадкове слово з файлу
             word = random.choice(f.readlines()).strip()
-        print(word)
-    p = flickr.photos.search(
+        print(word) 
+    p = flickr.photos.search(                                      # якась функція флікр для пошуку фото
         text=word,
         per_page=500,
         extras='url_o,o_dims,tags',
         safesearch=2,
     )
-    print(len(p['photos']['photo']))
-    photos = []
+    print(len(p['photos']['photo']))                               #довжина першого фото у сфотмованому списку
+    photos = []                                                    
     for i in p['photos']['photo']:
         try:
-            if int(i['width_o']) >= width-20 and int(i['height_o']) >= height-20:
+            if int(i['width_o']) >= width-20 and int(i['height_o']) >= height-20: #фільтрую фото за розміром і додаю в photos
                 photos.append(i)
         except:
             continue
     print(len(photos))
-    photo = random.choice(photos)
-    purl = photo['url_o']
-    if 'food' in photo['tags']:
+    photo = random.choice(photos)                                   # нова змінна photo
+    purl = photo['url_o']                                           # дістаємо url з фото і додаємо в pur1
+    if 'food' in photo['tags']:                                     # якщо фото з їжею - надрукувати що воно з їжею
         print('contains food')
         return None, None
-    pprint(photo)
-    r = requests.get(purl)
-    with open('fimage.jpg', 'wb') as f:
+    pprint(photo)                       
+    r = requests.get(purl)                                                                   
+    with open('fimage.jpg', 'wb') as f:                             # записую імідж у файл
         f.write(r.content)
     pic = Image.open('fimage.jpg')
     return pic, word
 
-def select_section(pic, width, height, b_width):
+def select_section(pic, width, height, b_width):                    # обрізаю фото
     w = width - (b_width * 2)
     h = height - (b_width * 2)
     x = [i for i in range(pic.size[0] - w)]
@@ -76,7 +76,7 @@ def select_section(pic, width, height, b_width):
     pic = pic.crop((left, top, right, bottom))
     return pic
 
-def get_font_size(word, width, height, margin):
+def get_font_size(word, width, height, margin):                     # підлаштовую шрифт до світлини
     margin = margin * 2
     size_w = width - margin
     size_h = height - margin
@@ -91,15 +91,15 @@ def get_font_size(word, width, height, margin):
         x -= 1
     return ImageFont.truetype('couture-bldit.ttf', x)
 
-def post_to_mastodon(pic_path, text):
+def post_to_mastodon(pic_path, text):                              # публікую в мастодон
     desc = "generated black and white image with the word {} in bold font overlaid".format(text)
     pic = mastodon.media_post(pic_path, description=desc)
     mastodon.status_post(text, media_ids=[pic])
 
-def post_to_twitter(pic_path, text):
+def post_to_twitter(pic_path, text):                              # публікую в тв
     twapi.update_with_media(pic_path, text)
 
-def cleanup():
+def cleanup():                                                    # підчищаю файли в директорії
     images = [i for i in os.listdir() if i.endswith(('.jpg', '.png', '.gif'))]
     for i in images:
         os.remove(i)
@@ -111,7 +111,7 @@ def cleanup():
 @click.option('--social/--nosocial', default=True)
 @click.option('--text', '-t', default='')
 @click.option('--search', '-z', default='')
-def main(palette, width, height, social, text, search):
+def main(palette, width, height, social, text, search): #<------------------------------------------ ТУТ MAIN
     palettes = {
         'classic': {
             'colorstr': '#000000',
@@ -144,8 +144,7 @@ def main(palette, width, height, social, text, search):
     if palette:
         color = palettes[palette]
     else:
-        color = palettes[random.choice(list(palettes.keys()))]
-
+        color = palettes[random.choice(list(palettes.keys()))]8г
 
     if height <= width:
         b_width = height // 40
